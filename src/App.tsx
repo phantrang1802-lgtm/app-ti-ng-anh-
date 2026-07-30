c846fef12d200ec978190fa1df840bd102e6ace3
Tên Của Bạn <email_cua_ban@example.com>
2026-07-30 14:57:09 +0700

    update: thay đổi nội dung trang chủ

--- src/App.tsx
+++ src/App.tsx
@@ -13,3 +13,20 @@ function App() {
 }
 
 export default App;
+notepad src\App.tsx
+notepad src\App.tsx
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Xin chào! Tôi là App Học Tiếng Anh</h1>
+        <p>Hãy bắt đầu học ngay hôm nay!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
