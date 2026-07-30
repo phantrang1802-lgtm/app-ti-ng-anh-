1df981c5acf4e4563c974938d8b6c031f32f46d7
Tên Của Bạn <email_cua_ban@example.com>
2026-07-30 15:56:46 +0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Xin chào! Tôi là App Học Tiếng Anh</h1>
-        <p>Hãy bắt đầu học ngay hôm nay!</p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-import React from "react";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+        <img 
+          src="/image.png" 
+          alt="English App" 
+          style={{ width: '300px', borderRadius: '10px' }} 
+        />
         <h1>Xin chào! Tôi là App Học Tiếng Anh</h1>
         <p>Hãy bắt đầu học ngay hôm nay!</p>
       </header>
